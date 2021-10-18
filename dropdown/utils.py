3fc3ae9bad0cf8080a96a16d18b2ae2dad96da66
Preeti Yuankrathok <preetisatit@gmail.com>
2021-10-18 16:40:01 +0700

    Remove unused function

--- dropdown/utils.py
+++ dropdown/utils.py
@@ -1,11 +1,2 @@
-import typing
-
-
 def dot_to_relation(value: str) -> str:
     return value.replace('.', '__')
-
-
-def extract_select_related(value: str) -> typing.Optional[str]:
-    split_value = value.split('.')
-
-    return '__'.join(split_value[:-1]) or None
